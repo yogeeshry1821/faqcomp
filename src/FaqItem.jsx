772abf8042b9f61d9f8c4ec046e540c2880401ad
Yogeesh <yryadappa100@gmail.com>
2024-12-17 12:45:25 +0530

    changes

--- src/FaqItem.jsx
+++ src/FaqItem.jsx
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react"
 
 const FaqItem = ({faq,index}) => {
-  return (
-    <div>
-        <div className="faq-question">{faq.question}</div>
-        <button className="faq-button">&gt;</button>
-        <div className="faq-answer">{faq.answer}</div>
+    const [show,setShow]=useState(false)
+    const handleClick=()=>{
+        setShow(!show);
+    }
+    useEffect((index)=>{
+        if(index ==0){
+            setShow(true);
+        }
+        console.log('lol')
+    },[]);
+    return (
+    <div className="sol-val">
+        <div className="faq-sol">
+                <button className={show ? "btn-rotate" : "faq-button"} onClick={handleClick}>&gt;</button>
+            <div>
+                <div className="faq-question">{faq.question}</div>
+                {show && <div className="faq-answer">{faq.answer}</div>}
+            </div>
+        </div>
     </div>
   )
 }
